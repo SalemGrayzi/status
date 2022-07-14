c3ff7c13d43ea65e0ad16d780fab89fe65261078
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-07-14 16:03:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from PIL import Image
-#from pandas_profiling import ProfileReport
 import streamlit.components.v1 as components
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -22,8 +21,7 @@ import hydralit_components as hc
 #when we import hydralit, we automatically get all of Streamlit
 import hydralit as hy
 
-
-app = hy.HydraApp(title='Simple Multi-Page App')
+app = hy.HydraApp(title='Diwan')
 
 
 
@@ -31,9 +29,9 @@ app = hy.HydraApp(title='Simple Multi-Page App')
 st.balloons()
 
 df= pd.read_csv('https://raw.githubusercontent.com/SalemGrayzi/status/main/Statuscsv.csv')
+df['Address'] =  df['Address'].fillna('بشامون')
 df.drop(['Order No_','Phone No_','Receipt No','Company'], axis = 1, inplace = True)
-df.dropna()
-df.drop_duplicates()
+df.drop_duplicates(inplace=True)
 #df['Amount'] = df.apply(lambda x: "{:,}".format(x['Amount']), axis=1)
 df['Handheld Used'] = df['Handheld Used'].map(
                    {True:'Used PDA' ,False:"Didn't Use PDA"})
@@ -48,10 +46,10 @@ Day=px.histogram(df, y= "Day Name",text_auto=True)
 driver=px.histogram(df, y="Driver Name", text_auto=True)
 driver.update_layout(yaxis={'categoryorder':'total ascending'})
 ####################################################################
-split_size = st.sidebar.slider('Top n Drivers', 0, 90, 5)
-dfd = df.groupby(['Driver Name']).size().to_frame().sort_values([0], ascending = False).head(split_size).reset_index()
-dfd.columns = ['Driver Name', 'count']
-drv = px.bar(dfd, y='Driver Name', x = 'count')
+#split_size = st.slider('Top n Drivers', 0, 90, 5)
+#dfd = df.groupby(['Driver Name']).size().to_frame().sort_values([0], ascending = False).head(split_size).reset_index()
+#dfd.columns = ['Driver Name', 'count']
+#drv = px.bar(dfd, y='Driver Name', x = 'count')
 ######################################
 vt=df['Handheld Used'].value_counts()
 vts=df['Handheld Used'].value_counts().index
@@ -76,14 +74,15 @@ stpk.update_layout(yaxis={'categoryorder':'total ascending'})
 am=df['Amount'].value_counts()
 op=df['OnlineApp'].value_counts()
 ops=df['OnlineApp'].value_counts().index
-onmount=go.Figure(data=[go.Pie(labels=df['OnlineApp'], values=df['Amount'], pull=[0.2, 0])])
+onmount=go.Figure(data=[go.Pie(labels=df['OnlineApp'], values=df.loc[df['Status'] == 'Delivered'].Amount, pull=[0.2, 0])])
 onmount.update_traces(textposition='inside', textinfo='percent+label')
+onmount2=go.Figure(data=[go.Pie(labels=df['OnlineApp'], values=df.loc[df['Status'] == 'Canceled'].Amount, pull=[0.2, 0])])
+onmount2.update_traces(textposition='inside', textinfo='percent+label')
 ######################################
-n_size = st.sidebar.slider('Top n Customers', 0, 90, 5)
-
-dfna = df.groupby("Name", as_index=False).sum().sort_values("Amount", ascending=False).head(n_size)
-amc=go.Figure(go.Bar(x=dfna["Amount"], y=dfna["Name"]))
-amc=px.histogram(data_frame=dfna, x='Amount', y='Name')
+#n_size = st.sidebar.slider('Top n Customers', 0, 90, 5)
+#dfna = df.groupby("Name", as_index=False).sum().sort_values("Amount", ascending=False).head(n_size)
+#amc=go.Figure(go.Bar(x=dfna["Amount"], y=dfna["Name"]))
+#amc=px.histogram(data_frame=dfna, x='Amount', y='Name')
 ######################################
 sto=px.histogram(df, y="Status", color="OnlineApp",text_auto=True)
 ######################################
@@ -91,8 +90,13 @@ tc=px.line(df, y=df['Time Created'].value_counts(),x=df['Time Created'].value_co
 ######################################
 tdc=px.line(df, y=df['Time to deploy'].value_counts(),x=df['Time to deploy'].value_counts().index)
 ######################################
-
-
+#slides = st.sidebar.slider('Top n Locations', 0, 90, 5)
+#addy = df.groupby(['Address']).size().to_frame().sort_values([0], ascending = False).head(slides).reset_index()
+#addy.columns = ['Adress', 'count']
+#addresss = px.bar(addy, y='Adress', x = 'count')
+######################################
+#dincome = px.histogram(df, y="Day ", color="Status",barnorm = "percent",hover_data=["Status"])
+#dincome.update_layout(yaxis={'categoryorder':'total ascending'})
 
 #PDA = st.sidebar.selectbox('Employee Related Analysis',
 #                                    ['None','Pickers','Picker and Order Status','PDA Usage','Drivers','All'])
@@ -135,70 +139,118 @@ tdc=px.line(df, y=df['Time to deploy'].value_counts(),x=df['Time to deploy'].val
 # Revenue and days needed to put in a selectbox
 
 
-chart_visual = st.sidebar.selectbox('Select Desired Graph',
-                                    ['None','Days','Drivers','PDA Usage',
-                                    'Pickers','Picker and Order Status', 'App vs. Call Revenues',
-                                    'Revenue Per Customer','Status of Delivery Using App','PDA and Status of Order'])
-
-if chart_visual == 'Days':
-    Day
-elif chart_visual == 'Drivers':
+#chart_visual = st.sidebar.selectbox('Select Desired Graph',
+#                                    ['None','Address','Days','Drivers','PDA Usage',
+#                                    'Pickers','Picker and Order Status', 'App vs. Call Revenues',
+#                                    'Revenue Per Customer','Status of Delivery Using App','PDA and Status of Order'])
+#
+#if chart_visual == 'Days':
+#    Day
+#elif chart_visual == 'Drivers':
 #    driver
-    drv
-elif chart_visual == 'PDA Usage':
-    pda
-elif chart_visual == 'Weekday/weekend':
-    weeks
-elif chart_visual == 'Pickers':
-    st.pyplot(pdapicker)
-elif chart_visual == 'Picker and Order Status':
-    stpk
-elif chart_visual == 'App vs. Call Revenues':
-    onmount
-elif chart_visual == 'Revenue Per Customer':
-    amc
-elif chart_visual == 'Status of Delivery Using App':
-    sto
-elif chart_visual == 'PDA and Status of Order':
-    st.pyplot(gh)
-elif chart_visual == 'None':
-    st.write(str(''))
+#    split_size = st.slider('Top n Drivers', 0, 90, 5)
+#    dfd = df.groupby(['Driver Name']).size().to_frame().sort_values([0], ascending = False).head(split_size).reset_index()
+#    dfd.columns = ['Driver Name', 'count']
+#    drv = px.bar(dfd, y='Driver Name', x = 'count')
+#    drv
+#elif chart_visual == 'PDA Usage':
+#    pda
+#elif chart_visual == 'Weekday/weekend':
+#    weeks
+#elif chart_visual == 'Pickers':
+#    st.pyplot(pdapicker)
+#elif chart_visual == 'Picker and Order Status':
+#    stpk
+#elif chart_visual == 'App vs. Call Revenues':
+#    onmount
+#    onmount2
+#elif chart_visual == 'Revenue Per Customer':
+#    n_size = st.slider('Top n Customers', 0, 90, 5)
+#    dfna = df.groupby("Name", as_index=False).sum().sort_values("Amount", ascending=False).head(n_size)
+#    amc=go.Figure(go.Bar(x=dfna["Amount"], y=dfna["Name"]))
+#    amc=px.histogram(data_frame=dfna, x='Amount', y='Name')
+#    amc
+#elif chart_visual == 'Status of Delivery Using App':
+#    sto
+#elif chart_visual == 'PDA and Status of Order':
+#    st.pyplot(gh)
+#elif chart_visual == 'Address':
+#    addresss
+#elif chart_visual == 'None':
+#    st.write(str(''))
 
 
 
 @app.addapp(is_home=True,icon='🏪')
 def Home():
  hy.info('Hello from app1')
+ st.write('hello this is the intro')
  st.title('Diwan Delivery Analysis')
  head = st.checkbox('First Few Rows')
  amount = st.checkbox("Amount Received in LBP")
+ if st.checkbox('Show all graphs'):
+    st.subheader('All Graphs')
+    Day
+    driver
+    split_size = st.slider('Top n Drivers', 0, 90, 5)
+    dfd = df.groupby(['Driver Name']).size().to_frame().sort_values([0], ascending = False).head(split_size).reset_index()
+    dfd.columns = ['Driver Name', 'count']
+    drv = px.bar(dfd, y='Driver Name', x = 'count')
+    drv
+    pda
+    st.pyplot(gh)
+    st.pyplot(pdapicker)
+    stpk
+    onmount
+    onmount2
+    n_size = st.slider('Top n Customers', 0, 90, 5)
+    dfna = df.groupby("Name", as_index=False).sum().sort_values("Amount", ascending=False).head(n_size)
+    amc=go.Figure(go.Bar(x=dfna["Amount"], y=dfna["Name"]))
+    amc=px.histogram(data_frame=dfna, x='Amount', y='Name')
+    amc
+    sto
+    tc
+    tdc
+    slides = st.slider('Top n Locations', 0, 90, 5)
+    addy = df.groupby(['Address']).size().to_frame().sort_values([0], ascending = False).head(slides).reset_index()
+    addy.columns = ['Adress', 'count']
+    addresss = px.bar(addy, y='Adress', x = 'count')
+    addresss
 
  if amount:
-     st.write("Amount received" , df['Amount'].sum(axis=0) , "in LBP from customers")
+     st.write("Amount received" , df.loc[df['Status'] == 'Delivered'].Amount.sum() , "in LBP from customers" , "and lost sales due to cancelation", df.loc[df['Status'] == 'Canceled'].Amount.sum())
  if head:
      st.write(df.head())
 
 @app.addapp(title='Employee Related Analysis',icon='💼')
 def app2():
  hy.info('Hello from app1')
+
  PDA1 = hy.selectbox('Employee Related Analysis',
                                     ['None','Pickers','Picker and Order Status','PDA Usage','Drivers','PDA and Status of Order','All'])
  if PDA1 == 'Pickers':
     st.pyplot(pdapicker)
+    st.write('this is what pickers are')
  elif PDA1 == 'PDA Usage':
         pda
  elif PDA1 == 'Picker and Order Status':
         stpk
  elif PDA1 == 'Drivers':
 #        driver
-        split_size
+        split_size = st.slider('Top n Drivers', 0, 90, 5)
+        dfd = df.groupby(['Driver Name']).size().to_frame().sort_values([0], ascending = False).head(split_size).reset_index()
+        dfd.columns = ['Driver Name', 'count']
+        drv = px.bar(dfd, y='Driver Name', x = 'count')
         drv
  elif PDA1 == 'PDA and Status of Order':
         st.pyplot(gh)
  elif PDA1 == 'All':
         st.pyplot(pdapicker)
         driver
-        split_size
+        split_size = st.slider('Top n Drivers', 0, 90, 5)
+        dfd = df.groupby(['Driver Name']).size().to_frame().sort_values([0], ascending = False).head(split_size).reset_index()
+        dfd.columns = ['Driver Name', 'count']
+        drv = px.bar(dfd, y='Driver Name', x = 'count')
         drv
         pda
         stpk
@@ -217,10 +269,12 @@ def app3():
 
  if App == 'App vs. Call Revenues':
      onmount
+     onmount2
  elif App == 'Status of Delivery Using App':
      sto
  elif App == 'All':
      onmount
+     onmount2
      sto
  elif App == 'None':
      st.write(str(''))
@@ -229,9 +283,13 @@ def app3():
 def app4():
  hy.info('Hello from app 2')
  App = hy.selectbox('Customer Analysis',
-                                     ['None','Days','Revenue Per Customer','deploy','time','All'])
+                                     ['None','Days','Revenue Per Customer','deploy','time','Address','All'])
 
  if App == 'Revenue Per Customer':
+     n_size = st.slider('Top n Customers', 0, 90, 5)
+     dfna = df.groupby("Name", as_index=False).sum().sort_values("Amount", ascending=False).head(n_size)
+     amc=go.Figure(go.Bar(x=dfna["Amount"], y=dfna["Name"]))
+     amc=px.histogram(data_frame=dfna, x='Amount', y='Name')
      amc
  elif App == 'deploy':
      tdc
@@ -239,11 +297,26 @@ def app4():
      tc
  elif App == 'Days':
      Day
+ elif App == 'Address':
+     slides = st.slider('Top n Locations', 0, 90, 5)
+     addy = df.groupby(['Address']).size().to_frame().sort_values([0], ascending = False).head(slides).reset_index()
+     addy.columns = ['Adress', 'count']
+     addresss = px.bar(addy, y='Adress', x = 'count')
+     addresss
  elif App == 'All':
+     n_size = st.slider('Top n Customers', 0, 90, 5)
+     dfna = df.groupby("Name", as_index=False).sum().sort_values("Amount", ascending=False).head(n_size)
+     amc=go.Figure(go.Bar(x=dfna["Amount"], y=dfna["Name"]))
+     amc=px.histogram(data_frame=dfna, x='Amount', y='Name')
      amc
      tdc
      tc
      Day
+     slides = st.slider('Top n Locations', 0, 90, 5)
+     addy = df.groupby(['Address']).size().to_frame().sort_values([0], ascending = False).head(slides).reset_index()
+     addy.columns = ['Adress', 'count']
+     addresss = px.bar(addy, y='Adress', x = 'count')
+     addresss
  elif App == 'None':
      st.write(str(''))
 
@@ -252,18 +325,3 @@ def app4():
 
 #Run the whole lot, we get navbar, state management and app isolation, all with this tiny amount of work.
 app.run()
-
-if st.sidebar.checkbox('Show all graphs'):
-    st.subheader('All Graphs')
-    Day
-    driver
-    drv
-    pda
-    st.pyplot(gh)
-    st.pyplot(pdapicker)
-    stpk
-    onmount
-    amc
-    sto
-    tc
-    tdc
