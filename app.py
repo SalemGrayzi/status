fba96f198f81d59c4e20a6c1c3b6381602369fec
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 20:29:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import markdown
 from streamlit_metrics import metric, metric_row
 import time
 st.set_page_config(layout="wide")
-pi1, pi2,pi3 = st.columns(2) ### Adding columns to insert the picture in the middle of the screen in column 2
+pi1, pi2 = st.columns(2) ### Adding columns to insert the picture in the middle of the screen in column 2
 
 with pi1:
     st.image("https://play-lh.googleusercontent.com/qPmIH0OemtPoTXyEztnpZVW-35sEWvrw99DIX6n1sklf1mDekUxtMzyInpJlTOATsp5B",width=125)
