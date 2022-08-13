5b74f130a391fc2b7db77ebe23810cadf656ed0f
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 20:26:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -524,6 +524,6 @@ page_names_to_funcs = {
     "Queuing Model ⌚": page5,
     "About 🤵": page6}
 
-selected_page = st.selectbox("Select a page", page_names_to_funcs.keys())
+selected_page = pi2.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
 
