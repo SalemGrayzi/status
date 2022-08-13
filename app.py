99a71ef95d5474a8c3e036aca800cf98add525ef
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 17:15:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,12 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from PIL import Image
 import streamlit.components.v1 as components
 import seaborn as sns
 import matplotlib.pyplot as plt
 import markdown
 import hydralit_components as hc
-import time
 from streamlit_metrics import metric, metric_row
-import io
 import hydralit as hy
 
 
