ea4a1d4485e3f2aea9fc027e54fad26b06ed064b
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 20:20:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(layout="wide")
 pi1, pi2,pi3 = st.columns(3) ### Adding columns to insert the picture in the middle of the screen in column 2
 
 with pi1:
-    st.image("https://play-lh.googleusercontent.com/qPmIH0OemtPoTXyEztnpZVW-35sEWvrw99DIX6n1sklf1mDekUxtMzyInpJlTOATsp5B",width=200)
+    st.image("https://play-lh.googleusercontent.com/qPmIH0OemtPoTXyEztnpZVW-35sEWvrw99DIX6n1sklf1mDekUxtMzyInpJlTOATsp5B",width=125)
 with pi2:
     st.write(' ') # Adding Diwan hyper market picture
 with pi3:
