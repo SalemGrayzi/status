f41b1d8ef419f9b2c511cac1d0ddece52f8b1ba0
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 17:48:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from hydralit import HydraApp
+
 import streamlit as st
 
-app = HydraApp(title='Sample Hydralit App',favicon="🐙")
-  
-#add all your application classes here
-app.add_app("Small App", icon="🏠", app=MySmallApp())
-app.add_app("Sample App",icon="🔊", app=MySampleApp())
-
-    #run the whole lot
-app.run()
+st.write('hiii')
