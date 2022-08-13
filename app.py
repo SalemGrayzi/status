7e709c2e04f5c626c78bede5e50bd2fbb05003dd
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 17:45:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit.components.v1 as components
 import seaborn as sns
 import matplotlib.pyplot as plt
 import markdown
-from hydralit import HydraHeadApp
+from hydralit import HydraHeadApp as hy
 import time
 from streamlit_metrics import metric, metric_row
 import io
