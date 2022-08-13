7547ba2021f459a392911911ef3f136244ca3d4e
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-13 18:20:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,35 +21,6 @@ for percent_complete in range(100):
     my_bar.progress(percent_complete + 1)
 st.write('Done Loading')
 
-st.markdown(
-    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
-    unsafe_allow_html=True,
-)
-query_params = st.experimental_get_query_params()
-tabs = ["Home", "About", "Contact","Home1", "About1", "Contact1"]
-if "tab" in query_params:
-    active_tab = query_params["tab"][0]
-else:
-    active_tab = "Home"
-
-if active_tab not in tabs:
-    st.experimental_set_query_params(tab="Home")
-    active_tab = "Home"
-
-li_items = "".join(
-    f"""
-    <li class="nav-item">
-        <a class="nav-link{' active' if t==active_tab else ''}" href="/?tab={t}">{t}</a>
-    </li>
-    """
-    for t in tabs
-)
-tabs_html = f"""
-    <ul class="nav nav-tabs">
-    {li_items}
-    </ul>
-"""
-
 st.markdown(tabs_html, unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 # Contents of ~/my_app/streamlit_app.py
@@ -62,7 +33,7 @@ def main_page():
 def page2():
     st.markdown("# Page 2 ❄️")
     st.sidebar.markdown("# Page 2 ❄️")
-
+    st.write('12165')
 def page3():
     st.markdown("# Page 3 🎉")
     st.sidebar.markdown("# Page 3 🎉")
@@ -75,3 +46,4 @@ page_names_to_funcs = {
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
+
