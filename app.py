8322ba734daa9daec2a041ea50cc602d7d5a81a0
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-14 16:06:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -124,6 +124,8 @@ sto.update_layout(title="Status of Order per Ordering Method",xaxis_title="",yax
 st.cache()
 df['Time Created'] = pd.to_datetime(df['Time Created'], format='%I:%M:%S %p')
 tc=px.histogram(x=df['Time Created'])
+tc.update_layout(title="Time Created of Orders",xaxis_title="Time in 24 Hour Format",yaxis_title="")
+
 
 #,category_orders={'':["7:00:00 AM","8:00:00 AM","9:00:00 AM", "10:00:00 AM", "11:00:00 AM", "12:00:00 PM","1:00:00 PM","2:00:00 PM","3:00:00 PM","4:00:00 PM","5:00:00 PM","6:00:00 PM","7:00:00 PM","8:00:00 PM","9:00:00 PM","10:00:00 PM","11:00:00 PM","12:00:00 PM","1:00:00 AM","2:00:00 AM","3:00:00 AM","4:00:00 AM","5:00:00 AM","6:00:00 AM"]})
 
