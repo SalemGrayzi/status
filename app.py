3243da4230ac9362fd8d05d6512b5fe8ceceed7b
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-18 14:45:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,6 @@ df['OnlineApp'] = df['OnlineApp'].map(
 df['Time Created'] = pd.to_datetime(df['Time Created'], format='%I:%M:%S %p')
 
 ### In the bellow section it contains all the graphs made
-from datetime import timedelta
-
-times = df['Time Created']
-print(str(timedelta(seconds=sum(map(lambda f: int(f[0])*3600 + int(f[1])*60 + int(f[2]), map(lambda f: f.split(':'), times)))/len(times))))
 
 ###################################### Graph to get orders per day in a year
 st.cache()
