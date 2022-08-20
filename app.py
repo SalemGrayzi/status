291a4b893931feda6c0e16997d34880bf899378c
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-20 20:17:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,14 @@ st.set_page_config(layout="wide")
 ### Setting picture to dashboard and resizing it
 st.image("https://play-lh.googleusercontent.com/qPmIH0OemtPoTXyEztnpZVW-35sEWvrw99DIX6n1sklf1mDekUxtMzyInpJlTOATsp5B",width=100)
 
-### Building the HydraApp
-
+with st.expander("Upload Data"):
+     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+     if uploaded_file is None:
+        df= pd.read_csv('https://github.com/SalemGrayzi/status/blob/main/Statuscsv.csv?raw=true')
+      else:
+         df = pd.read_csv(uploaded_file)
 ### Importing csv file from github onto streamlit
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-if uploaded_file is None:
-  df= pd.read_csv('https://github.com/SalemGrayzi/status/blob/main/Statuscsv.csv?raw=true')
-else:
-  df = pd.read_csv(uploaded_file)
 
 ### Filling missing values in Adress column with the mode
 df['Address'] =  df['Address'].fillna('بشامون')
