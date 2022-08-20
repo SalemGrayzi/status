3886aeffb76e1311141d6ca585c401a2d6e47691
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-08-20 16:27:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,14 @@ from millify import millify
 st.set_page_config(layout="wide")
 
 ### Importing csv file from github onto streamlit
-df= pd.read_csv('https://github.com/SalemGrayzi/status/blob/main/Statuscsv.csv?raw=true')
+#df= pd.read_csv('https://github.com/SalemGrayzi/status/blob/main/Statuscsv.csv?raw=true')
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is None:
+  df= pd.read_csv('https://github.com/SalemGrayzi/status/blob/main/Statuscsv.csv?raw=true')
+else:
+  df = pd.read_csv(uploaded_file)
 
 ### Filling missing values in Adress column with the mode
 df['Address'] =  df['Address'].fillna('بشامون')
